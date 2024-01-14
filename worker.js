718f2d2d3e42f835208aaab701b5535a9a381034
Viktor Petersson <self@vpetersson.com>
2024-01-14 12:14:15 +0000

    Adds worker.js

---
+++ worker.js
@@ -0,0 +1,24 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    // Hardcoded URL mappings
+    const mappings = {
+        "/twitter": "https://twitter.com/vpetersson",
+        "/linkedin": "https://www.linkedin.com/in/vpetersson/",
+        "/nowv-apple": "https://podcasts.apple.com/us/podcast/nerding-out-with-viktor/id1722663295",
+        "/nowv-spotify": "https://podcasters.spotify.com/pod/show/nerding-out-with-viktor",
+        "/nowv-youtube": "https://www.youtube.com/@nerdingoutwithviktor",
+    };
+
+    const target = mappings[path];
+    if (target) {
+        return Response.redirect(target, 301);
+    }
+
+    return new Response('URL not found', { status: 404 });
+}
